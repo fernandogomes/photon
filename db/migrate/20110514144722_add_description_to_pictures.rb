96248af894e573a4364ef0b9c5d182c89f32980e
Marc Lagrange <rhaamo@gruik.at>
2011-05-14 16:55:54 +0200

    Add picture description

---
+++ db/migrate/20110514144722_add_description_to_pictures.rb
@@ -0,0 +1,9 @@
+class AddDescriptionToPictures < ActiveRecord::Migration
+  def self.up
+    add_column :pictures, :description, :text
+  end
+
+  def self.down
+    remove_column :pictures, :description
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110513151356) do
+ActiveRecord::Schema.define(:version => 20110514144722) do
 
   create_table "albums", :force => true do |t|
     t.string   "name",        :null => false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(:version => 20110513151356) do
     t.integer  "album_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "roles", :force => true do |t|
